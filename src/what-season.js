980a8ce32c336f8274a337116009534225ad32c5
warlock150@gmail.com <email@example.com>
2023-04-16 20:40:54 +0200

    what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,24 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/*date*/) {
-  /*let inputDate = new Date(date);
-  if (Date.parse(inputDate) == NaN) {
+function getSeason(date) {
+  let inputDate = date;
+  if (inputDate === undefined) {
     return 'Unable to determine the time of year!';
   }
+  if (typeof(inputDate) === 'string' || isNaN(Date.parse(inputDate))) {
+    throw new Error('Invalid date!');
+  }
+
+    if (!inputDate.toISOString()) {
+     throw new Error('Invalid date!');
+    }
+
   let month = inputDate.getMonth();
-  if (month === 11 || month >=0 || month < 2 || date == 'winter') {
+  if (month === 11 || month >=0 && month < 2) {
     console.log(month);
     return 'winter';
-  } else if (month >=2 || month < 5){
+  } else if (month >=2 && month < 5){
     console.log(month);
     return 'spring';
-  } else if (month >=5 || month < 8 || date == 'summer'){
+  } else if (month >=5 && month < 8 || date == 'summer'){
     return 'summer';
-  } else if (month >=8 || month < 11){
+  } else if (month >=8 && month < 11){
     return 'autumn';
-  }*/
-  throw new NotImplementedError('Not implemented');
+  }
+ // throw new NotImplementedError('Not implemented');
 // remove line with error and write your code here
 }
 
